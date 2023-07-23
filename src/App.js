65cedab9c8f6d4305a40a38b91d8667ec2b0c210
Aourl24 <awwalmustapha13@gmail.com>
2023-07-23 20:40:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import "./css/bootstrap/bootstrap.min.css";
+import "./css/acss/acss.css";
+import "./css/acss/animate.min.css";
+import "./css/fontawesome_5/css/all.min.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="heading">Awwal don try</h1>
+      <h4 className="sub-heading">
+        
+      </h4>
+    </div>
+  );
+};
+ 
+export default App;
